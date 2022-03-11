105ac1a016317015a9513126c23de6e8e9042392
Rafael Pereira <rafaelpdemelo@gmail.com>
2022-03-11 16:47:06 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,52 @@
+// // CODE FOR POST METHOD
+
+const form = document.getElementById('formPost');
+form.onclick=fetchPost
+
+function fetchPost(e){ 
+
+  e.preventDefault();
+
+  let articleBody = document.getElementById("articleBody");
+  let headLine = document.getElementById("headLine");
+
+  fetch(
+    "http://localhost:8080/o/headless-delivery/v1.0/sites/20123/blog-postings/",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Basic " + btoa("test@liferay.com:test"),
+      },
+
+      body: JSON.stringify({
+        articleBody: `${articleBody.value}`,
+        headline: `${headline.value}`
+      }),
+    }
+  )
+    .then((resposta) => resposta.json())
+    .then((data) => console.log(data))
+  };
+
+//-------------------------------------------------------------------------------------------
+
+//CODE FOR GET METHOD
+function fetchGet(){
+  fetch(
+    "http://localhost:8080/o/headless-delivery/v1.0/sites/20123/blog-postings",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Basic " + btoa("test@liferay.com:test"),
+      },
+    }
+  )
+    .then((resposta) => resposta.json())
+    .then((data) => console.log(data))
+};
+
+//CODE FOR GET BY ID 
+
+// console.log(fetch('http://localhost:8080/o/headless-delivery/v1.0/sites/20123/blog-postings'))
